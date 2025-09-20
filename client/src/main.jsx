60a79dfbaa9d24bf1805b3fb60db55a62124cfa4
sensei-amit <geniusamit.v@gmail.com>
2025-09-21 02:15:24 +0530

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,25 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { AppContextProvider } from './context/AppContext.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+import React from 'react'
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY||"pk_test_ZGVsaWNhdGUtcmFiYml0LTcuY2xlcmsuYWNjb3VudHMuZGV2JA"
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+createRoot(document.getElementById('root')).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+  <BrowserRouter>
+  <AppContextProvider >
+    <App />
+   </AppContextProvider>
+  </BrowserRouter>
+  </ClerkProvider>,
+  
+)
